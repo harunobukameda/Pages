ca79a50884108fb9efead90198b81f9772a4efc8
harunobukameda <harunobukameda@gmail.com>
2023-03-06 10:17:38 +0900

    test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,8 +3,6 @@ export const config = {
 }
 
 export default async function (req) {
-  console.log(await [process.env.KV.get('test')])
-  console.log(process.env.MY_ENV_VAR)
   console.log(await process.env.KV.get('test'))
 
   return Response.json({ name: 'John Doe' })
