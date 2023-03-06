777f85e6995a979320af0bb9bbc9332ec37e84fa
harunobukameda <harunobukameda@gmail.com>
2023-03-06 10:47:47 +0900

    test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@ export const config = {
   runtime: 'edge',
 }
 
-export default async function (req) {
+export default async function getServerSideProps() {
   console.log(await process.env.KV.get('test'))
   const kvvalue = await process.env.KV.get('test')
   return new Response(kvvalue)
