7b4559965a63dce16a1c62da40ca36e94e30f12d
harunobukameda <harunobukameda@gmail.com>
2023-03-06 13:22:59 +0900

    test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@ export const config = {
   runtime: 'edge',
 }
 
-export default async function getStaticProps() {
-  const random = Math.floor(Math.random() * 100);
-  return new Response(random);
+export default async function (req) {
+  const kvvalue = await process.env.KV.get('test')
+  return new Response(kvvalue)
 }
